66ef429d3d9ef7bc4229879c792857f2782f6423
Parrot User <user@localhost.localdomain>
2022-02-21 20:00:06 +0000

    Old code removed

--- src/app.py
+++ src/app.py
@@ -12,12 +12,8 @@ def login_get():
 @app.route('/', methods=['POST',])
 def login_post():
     username = request.form.get('username')
-    password = request.form.get('password')
     template = '{% extends "login.html" %}{% block response %}User ' + username + ' does not exist.{% endblock %}'
-
-    message = "User {} does not exist".format(username)
     return render_template_string(template, data=username)
-    return render_template("login.html", data=username)
 
 from datetime import datetime
 
